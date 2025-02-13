431df654bf1059718e4e3cf9539b2202402d4ad1
Shane-Dias <160272459+Shane-Dias@users.noreply.github.com>
2025-02-13 21:10:00 +0530

    Easy level

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Q1 Reverse a String
 function reverseString() {
   let str = document.getElementById("reverse").value;
-  console.log(`String is:${str}`)
+  console.log(`String is:${str}`);
   let newStr = str.split("").reverse().join("");
   document.getElementById("result1").value = newStr;
 }
@@ -10,22 +10,24 @@ function reverseString() {
 // console.log(reverseString("world"));
 
 // Q2 Check for Palindrome
-function isPalindrome(str) {
+function isPalindrome() {
+  let str = document.getElementById("palindrome").value;
   str = str.toLowerCase();
-  return str === str.split("").reverse().join("");
+  // return str === str.split("").reverse().join("");
+  if (str === str.split("").reverse().join("")) {
+    document.getElementById("result2").value = "Ture";
+  } else {
+    document.getElementById("result2").value = "False";
+  }
 }
 
 // console.log(isPalindrome("racecar"));
 // console.log(isPalindrome("hello"));
 
 //Q3 Find the Largest Number in an Array
-function largestInArray() {
-  let arr = [10, 20, 30, 40];
-  let max = arr[0];
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > max) {
-      max = arr[i];
-    }
-  }
-  console.log(max);
+function findLargest() {
+  let arr = document.getElementById("largestEle").value;
+  let numArr = arr.split(",").map(Number); 
+  let largest = Math.max(...numArr); 
+  document.getElementById("result3").value = "Largest number: " + largest;
 }
